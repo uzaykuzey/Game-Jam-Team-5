f4c2523a5b17845ebd3a119c296d509f347d14dc
Uzay Kuzey <atauzayk@gmail.com>
2024-02-16 22:42:38 +0300

    health stuff 2

---
+++ Game Jam Team Jam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] public Transform[] healthBars;
+    [SerializeField] public Transform cameraTransform;
+    [SerializeField] public Camera cameraMain;
+    private void Update()
+    {
+        float distanceToCamera = 2f;
+        Vector3 offset1 = new Vector3(-7.95119f, 4.396813f, distanceToCamera);
+        Vector3 offset2 = new Vector3(-6.750601f, 4.376113f, distanceToCamera);
+        Vector3 offset3 = new Vector3(-5.529312f, 4.376113f, distanceToCamera);
+
+        healthBars[0].position = cameraTransform.position + offset1;
+        healthBars[1].position = cameraTransform.position + offset2;
+        healthBars[2].position = cameraTransform.position + offset3;
+    }
+}
