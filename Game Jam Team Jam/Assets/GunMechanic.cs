f8abb4c3832705019df4eaa67af000e517be4368
Ege <114308374+egeslittlehelper@users.noreply.github.com>
2024-02-17 14:56:10 +0300

    ersrsre

--- Game Jam Team Jam/Assets/GunMechanic.cs
+++ Game Jam Team Jam/Assets/GunMechanic.cs
@@ -15,6 +15,9 @@ public class GunMechanic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(Input.GetKeyDown(KeyCode.x))
+        if(Input.GetKeyDown(KeyCode.x))
+        {
+            
+        }
     }
 }
