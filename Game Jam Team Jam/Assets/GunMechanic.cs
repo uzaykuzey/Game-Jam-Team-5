4b7c3fa1a74a15e7f5a465fd7942c6a76fc5c41d
Ege <114308374+egeslittlehelper@users.noreply.github.com>
2024-02-17 14:48:27 +0300

    weqwweqwe

---
+++ Game Jam Team Jam/Assets/GunMechanic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMechanic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.x))
+    }
+}
