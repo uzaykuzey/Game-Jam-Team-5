d7cb6cf3abee7575ff30b293d107c7493c13fc61
Ege <114308374+egeslittlehelper@users.noreply.github.com>
2024-02-17 14:13:53 +0300

    genel

--- Game Jam Team Jam/Assets/FollowPlayer.cs
+++ Game Jam Team Jam/Assets/FollowPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f,0f,-10f);
+    private Vector3 offset = new Vector3(5f,2f,-10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
     [SerializeField] private Transform target;
@@ -13,7 +13,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPosition = target.poisiton + offset;
+        Vector3 targetPosition = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity,smoothTime);
     }
 }
